6c7b25304ce9c6ee371f6c31d0a4ba96f6ac751a
Nanaky <rokoko1919a@gmail.com>
2026-06-15 12:09:51 +0100

    added recipe and pin logic

--- src/main/java/net/nanaky/ultimate_map_atlases/UltimateMapAtlases.java
+++ src/main/java/net/nanaky/ultimate_map_atlases/UltimateMapAtlases.java
@@ -1,16 +1,23 @@
 package net.nanaky.ultimate_map_atlases;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 import net.nanaky.ultimate_map_atlases.lifecycle.MapAtlasesServerEvents;
 import net.nanaky.ultimate_map_atlases.utils.BlockMarkerEvents;
+
 
 public class UltimateMapAtlases implements ModInitializer {
     @Override
     public void onInitialize() {
         MapAtlasesMod.init();
         BlockMarkerEvents.register();
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> {
+            content.insertAfter(Items.MAP, MapAtlasesMod.MAP_ATLAS.get());
+        });
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             for (var p : server.getPlayerList().getPlayers()) {
                 MapAtlasesServerEvents.onPlayerTick(p);
